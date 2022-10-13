d8441912688911163f3d6afcf3a5998b3a392c60
Irfan Ali <114615634+Irfanaliqureshi@users.noreply.github.com>
2022-10-13 14:33:58 +0500

    Add files via upload

---
+++ Lab 4.4.c
@@ -0,0 +1,12 @@
+//lab task 4.4 
+#include<stdio.h>
+ #include<conio.h>
+ int main() 
+{
+ 	int a,b; 
+	printf("Enter Any Two Integers: ");
+ 	scanf("%d %d",&a,&b); 
+	if(a>b) 	printf("%d is Maximum",a);
+ 	else 	printf("%d is Maximum",b); 	
+return 0; 
+}
