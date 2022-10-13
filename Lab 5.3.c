94cf21153742a52cb7820d121a9186734f3b2106
Irfan Ali <114615634+Irfanaliqureshi@users.noreply.github.com>
2022-10-13 15:21:41 +0500

    Create Lab 5.3.c

---
+++ Lab 5.3.c
@@ -0,0 +1,21 @@
+//lab 5.3
+#include<stdio.h> 
+#include<conio.h> 
+int main()
+ { 
+	char a,A;
+ 	a='a'; 
+	while(a<='z')
+ 	{ 
+		printf("%c ",a);
+ 		a++; 
+	} 
+	printf("\n"); 
+	A='A'; 
+	while(A<='Z') 
+	{ 	
+	printf("%c ",A); 	
+	A++; 
+	}	 
+return 0;	
+ }
