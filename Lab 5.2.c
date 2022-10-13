4e87469299a0933c6bfdd13ed209ca81ab58a97e
Irfan Ali <114615634+Irfanaliqureshi@users.noreply.github.com>
2022-10-13 15:20:32 +0500

    Create Lab 5.2.c

---
+++ Lab 5.2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ #include<conio.h>
+ int main()
+ {
+ 	int i,num; 
+	printf("Enter The Range For The Reverse Order OF Odd Numbers: "); 
+	scanf("%d",&i);
+ 	while(i>=0) 
+	{ 	
+	printf("%d ",i); 		i=i-2; 	
+	 	
+ 	} 
+return 0;	 
+}
